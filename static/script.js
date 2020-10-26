62d36d65d61c2af68f75dd5d23821f8f2f4d1f6a
aditya41 <aditya41@users.noreply.github.com>
2020-10-26 15:46:48 +0530

    optimised

--- static/script.js
+++ static/script.js
@@ -1,19 +1,20 @@
 $(() => {
 
     function refresh() {
-        // $('#list').empty()
+        $('#list').empty()
         $.get('/todo', (data) => {
+            let lis = []
             for (let i = 0; i < data.length; i++) {
-                $('#list').append(
-                    $('<li>').text(data[i])
-                )
+                lis.push(`<li>${data[i]}</li>`)
             }
+            $('#list').append(lis)
         })
     }
     refresh()
 
     $('#btn').click(() => {
         let task = $('#inp').val()
+        $('#inp').val("")
             // console.log(task)
         $.get(`/addtodo?task=${task}`)
         refresh()
